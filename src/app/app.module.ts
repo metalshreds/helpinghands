1a0f92a5462f99846f5540cc797bcb32ab4f78c7
Song Zhao <lanmovii@gmail.com>
2017-10-03 19:54:05 -0500

    Revert "login UI"
    This reverts commit 8c3bb053789e6a8c892711c24f73e3465e6f9e7a.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,15 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-import { LoginPage } from '../pages/login/login';
-import { SignupPage } from '../pages/signup/signup';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { AuthProvider } from '../providers/auth/auth';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage,
-    LoginPage,
-    SignupPage
+    ListPage
   ],
   imports: [
     BrowserModule,
@@ -27,14 +23,12 @@ import { AuthProvider } from '../providers/auth/auth';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage,
-    LoginPage
+    ListPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    AuthProvider
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
