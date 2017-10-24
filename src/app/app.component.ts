80c0650526840e5fcbfd2b05f17c5f36b45f2ac8
KiegsRoss <kross6@wisc.edu>
2017-10-24 18:50:18 -0500

    Dashboard View

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import {SignupPage} from "../pages/signup/signup";
 import firebase from 'firebase';
+import {DashboardPage} from "../pages/dashboard/dashboard";
 @Component({
   templateUrl: 'app.html'
 })
@@ -34,6 +35,7 @@ export class MyApp {
       { title: 'List', component: ListPage },
       { title: 'Login', component: LoginPage},
       { title: 'Sign up', component: SignupPage},
+      { title: 'Dashboard', component: DashboardPage}
     ];
 
   }
--- src/pages/pending/pending.ts
+++ src/pages/pending/pending.ts
@@ -14,10 +14,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'pending.html',
 })
 export class PendingPage {
+    tasks: Array<{title: string, time: string, location: string, description: string}>;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
 
+    this.tasks =[];
+    this.tasks.push({title: 'Coding Help',
+                     time: '5:00 pm',
+                     location: 'Union South',
+                     description:  'I need help figuring out how to parse files' +
+                     ' for my programming assignment' });
+
+    this.tasks.push({title: 'Impressive Karate',
+                  time: '8:00 am',
+                  location: '123 Some Street',
+                  description:  'I need someone to teach me basic karate ' +
+                  ' moves so I can impress my friend!' });
+
+}
   ionViewDidLoad() {
     console.log('ionViewDidLoad PendingPage');
   }
