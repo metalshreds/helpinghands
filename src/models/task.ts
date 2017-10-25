9e21c1f63897bcf885d2368af45b93bbb2d723c5
Muriel <mkelso9@gmail.com>
2017-10-24 20:00:41 -0500

    Task has all attributes from googledoc and completed tab creates a task and displays it

--- src/models/task.ts
+++ src/models/task.ts
@@ -1,13 +1,29 @@
 import { User } from './user';
 
 
-export interface Task {
+export class Task {
+
   //attributes for task.
+  constructor(
+    public timeDuration : number,
+    public timeStart : number,
+    public introduction : string,
+    public requirement : string,
+    public wantedSkill : boolean[],
+    public complete: boolean,
+    public id: number,
+    public name: string,
+    public helpers: User[],
+    public appliedHelpers: User[]
+  ){}
+
+
+  /*
   user : User;            //or put just put email attributes here
   timeDuration : number;
   timeStart : number;
   introduction : string;
   requirement : string;
   wantedSkill : string[];
-
+  */
 }
