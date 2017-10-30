70e3072dffe99ac8d2e4bdafc4c2b0d6cc0d7137
Joel Tanumihardjo <jltanumihard@wisc.edu>
2017-10-30 18:00:30 -0500

    Might be changing edit task look

--- src/pages/task-edit/task-edit.ts
+++ src/pages/task-edit/task-edit.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TaskObjectProvider } from '../../providers/task-object/task-object';
 
 /**
  * Generated class for the TaskEditPage page.
@@ -14,8 +15,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'task-edit.html',
 })
 export class TaskEditPage {
-
+  task: TaskObjectProvider;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    var skills = [true, false, true];
+    this.task = new TaskObjectProvider( "taskname1", 10, "startdate", "introduction", "requirement", skills, false, 'user1');
   }
 
   ionViewDidLoad() {
