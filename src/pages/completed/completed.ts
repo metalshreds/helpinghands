86fe819d70db8a438f9a3eb595100dc9b3b9bd72
Song Zhao <lanmovii@gmail.com>
2017-10-31 22:56:48 -0500

    z

--- src/pages/completed/completed.ts
+++ src/pages/completed/completed.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {User} from "../../models/user";
 import {TaskObjectProvider} from "../../providers/task-object/task-object";
+import {TaskViewPage} from "../task-view/task-view";
 
 /**
  * Generated class for the CompletedPage page.
@@ -17,22 +18,35 @@ import {TaskObjectProvider} from "../../providers/task-object/task-object";
 })
 export class CompletedPage {
 
-
   tasks: Array<TaskObjectProvider>;
   skills: Array<boolean>;
-  helpers: Array<User>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.tasks =[];
     this.skills = [];
-    this.helpers = [];
-    let task = new TaskObjectProvider( "taskname", 10, "startdate", "introduction", "requirment", this.skills, false, 'user1');
+
+    //creates example task
+    let task = new TaskObjectProvider( "Dumb As Dirt", 6.5,
+      "1:00 pm, April 20th", "I am trying to do an experiment that involves " +
+      "using dirts with different pH levels. On my first go, I couldn't get" +
+      " the right pH levels, so I am looking for someone who could get dirt" +
+      " with the right pH level.", "All you need to know is how to get the" +
+      " right pH level in dirt.", this.skills, true, 'Franklin');
+
+    //adds example task to list of tasks
     this.tasks.push(task);
 
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CompletedPage');
+  }
+  //navigates to taskview page if task clicked
+  taskClicked(event, task) {
+    this.navCtrl.push(TaskViewPage, {
+      task: task
+    });
+
   }
 
 }
--- src/pages/suggested/suggested.ts
+++ src/pages/suggested/suggested.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {TaskObjectProvider} from '../../providers/task-object/task-object';
 import { User } from '../../models/user';
-
+import {TaskViewPage} from '../task-view/task-view';
 
 /**
  * Generated class for the SuggestedPage page.
@@ -19,21 +19,43 @@ import { User } from '../../models/user';
 export class SuggestedPage {
   tasks = Array<TaskObjectProvider>();
   skills: Array<boolean>;
-  helpers: Array<User>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.tasks =[];
-    this.skills = [];
-    this.helpers = [];
-    let taskA = new TaskObjectProvider( "taskname1", 10, "startdate", "introduction", "requirment", this.skills, false, 'user1');
-    let taskB = new TaskObjectProvider( "taskname2", 10, "startdate", "introduction", "requirment", this.skills, false, 'user1');
-    let taskC = new TaskObjectProvider( "taskname3", 10, "startdate", "introduction", "requirment", this.skills, false, 'user1');
+
     this.tasks = [];
+    this.skills = [];
+
+    //creating some example tasks
+    let taskA = new TaskObjectProvider("I Want To Be Lazy", 12, "11:00 am, " +
+      "May 9th", 'For my job, I need to take the name, date, and complaint ' +
+      'info from emails. I was hoping that someone could make a program ' +
+      'that would do that for me.', 'Must be able to code.', this.skills
+      , false, "Georgina");
+    let taskB = new TaskObjectProvider("The Name Escapes Me", 1, "2:45 pm, " +
+      "May 11th", 'There are some art pieces that I have created, but I ' +
+      'cannot figure out names for them. I was hoping someone could help' +
+      ' me come up with some. I will have pizza as well.', 'You should ' +
+      'be creative', this.skills, false, "Art");
+    let taskC = new TaskObjectProvider("I Need Freedom", 3, "8:35 am, May " +
+      "12th", "I have some handcuffs stuck on my hands. They are not there" +
+      " because I was arrested. I am totally not a criminal. Totally. What" +
+      " I need help with is getting them off.", "Must know how to remove" +
+      " handcuffs", this.skills, false, "TotallyNotACriminal");
+
+    //adding example tasks to task list
     this.tasks.push(taskA, taskB, taskC);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SuggestedPage');
+  }
+
+  //navigates to taskview page if task clicked
+  taskClicked(event, task) {
+    this.navCtrl.push(TaskViewPage, {
+      task: task
+    });
+
   }
 
 }
