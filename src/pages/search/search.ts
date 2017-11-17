900d23a618a7bb361fd3fafbf2b1cc1cf5c44138
Song Zhao <lanmovii@gmail.com>
2017-11-17 14:38:54 -0600

    synv

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import * as algoliasearch from 'algoliasearch';
 /**
  * Generated class for the SearchPage page.
  *
@@ -16,7 +16,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class SearchPage {
 
   items;
+  functions = require('firebase-functions')
+  ALGOLIA_ID = this.functions.config().algolia.app_id;
+  ALGOLIA_ADMIN_KEY = this.functions.config().algolia.api_key;
+  
+  ALGOLIA_INDEX_NAME = "notes";
+  client = algoliasearch(this.ALGOLIA_ID, this.ALGOLIA_ADMIN_KEY);
 
+  //https://github.com/firebase/functions-samples/blob/master/fulltext-search/functions/index.js
+  //https://stackoverflow.com/questions/45274485/how-to-integrate-algolia-in-ionic3
+  //https://github.com/algolia/algoliasearch-client-javascript
+  //https://firebase.google.com/docs/firestore/solutions/search?authuser=2
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.initializeItems();
 
