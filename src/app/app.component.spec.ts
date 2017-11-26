0df196c412f57ef0473b732b6eb9838feaac5d6d
Song Zhao <lanmovii@gmail.com>
2017-11-26 14:49:46 -0600

    jasmine fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,7 +3,7 @@ import { IonicModule } from 'ionic-angular';
 import { HelpingHands } from './app.component';
 import {ProfilePage} from "../pages/profile/profile";
 import {HomePage} from "../pages/home/home";
-
+import {} from 'jasmine';
 
 let comp: HelpingHands;
 let fixture: ComponentFixture<HelpingHands>;
