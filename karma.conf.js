23a6d32e2c3dbd23bf01095488654e7bdd0f7af3
Joel <jltanumihard@wisc.edu>
2017-11-26 12:42:42 -0600

    more karma/jasmine testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,41 +1,68 @@
-module.exports = function (config) {
- config.set({
-   basePath: '',
-   frameworks: ['jasmine', 'angular-cli'],
-   plugins: [
-     require('karma-jasmine'),
-     require('karma-chrome-launcher'),
-     require('karma-mocha-reporter'),
-     require('karma-remap-istanbul'),
-     require('angular-cli/plugins/karma')
-   ],
-   files: [
-     { pattern: './src/test.ts', watched: false }
-   ],
-   preprocessors: {
-     './src/test.ts': ['angular-cli']
-   },
-   mime: {
-     'text/x-typescript': ['ts','tsx']
-   },
-   remapIstanbulReporter: {
-     reports: {
-       html: 'coverage',
-       lcovonly: './coverage/coverage.lcov'
-     }
-   },
-   angularCli: {
-     config: './angular-cli.json',
-     environment: 'dev'
-   },
-   reporters: config.angularCli && config.angularCli.codeCoverage
-             ? ['mocha', 'karma-remap-istanbul']
-             : ['mocha'],
-   port: 9876,
-   colors: true,
-   logLevel: config.LOG_INFO,
-   autoWatch: true,
-   browsers: ['Chrome'],
-   singleRun: false
- });
-};
+// Karma configuration
+// Generated on Sun Nov 26 2017 12:40:59 GMT-0600 (Central Standard Time)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
